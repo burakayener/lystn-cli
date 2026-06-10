131ab27f533ccaa6997acc856171845e55c9efb4
Burak Akın Yener <108765766+burakayener@users.noreply.github.com>
2026-06-11 01:18:09 +0300

    lystn 0.1.3

--- Formula/lystn.rb
+++ Formula/lystn.rb
@@ -3,8 +3,8 @@ class Lystn < Formula
 
   desc "Listen to AI coding assistant responses through your speakers"
   homepage "https://github.com/burakayener/Lystn"
-  url "https://files.pythonhosted.org/packages/source/l/lystn/lystn-0.1.1.tar.gz"
-  sha256 "f2dfe11d83bdbc624a6edab55104d94bc45ee0a5fc7f41ff41a40ed3c8a4cbe5"
+  url "https://files.pythonhosted.org/packages/source/l/lystn/lystn-0.1.3.tar.gz"
+  sha256 "85bb258247245f35ad0931f1ba8ba09a2ac8667a33ed16f9198db4392cd55593"
   license "Apache-2.0"
 
   depends_on "portaudio"
